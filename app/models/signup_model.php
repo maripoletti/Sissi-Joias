ea50cc97ccaff0f22730300cad9a3f29d8249060
Maria Eduarda Poletti <m.dudapoletti@gmail.com>
2026-02-25 20:11:40 -0300

    arrumações

--- app/models/signup_model.php
+++ app/models/signup_model.php
@@ -11,6 +11,29 @@ class Signup_model extends Dbh {
 
         try {
             $query = "INSERT INTO Auth_Users"
+            $query = "INSERT INTO Auth_Users (Pwd, Email) VALUES (:pwd, :email);";
+            $stmt-> bindParam(":pwd", $pwd);
+            $stmt-> bindParam(":email", $email);
+            $stmt-> execute();
+
+            $UserID = $pdo->lastInsertId();
+
+            $query = "INSERT INTO Sales_Employees (UserID, FullName, Phone) VALUES (:userid, :fullname, :phone);";
+            $stmt = $pdo->prepare($query);
+            $stmt->bindParam(":userid", $UserID);
+            $stmt->bindParam(":fullname", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt-> execute();
+
+            $query = "INSERT INTO Auth_UserRoles (UserID, RoleID) VALUES (:userid, 2)";
+            $stmt = $pdo->prepare($query);
+            $stmt->bindParam(":userid", $UserID);
+            $stmt->execute();
+
+            $pdo->commit();
+        } catch (PDOException $e) {
+            $pdo->rollback();
+            echo "Erro na conexão: " . $e->getMessage();
         }
     }
 }
