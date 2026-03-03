eee26393f836ad6db60e6b3b611d3d846dc7c1f5
Maria Eduarda Poletti <m.dudapoletti@gmail.com>
2026-03-02 23:30:17 -0300

    Update vendas.php
    arrumando o painel de controle

---
+++ public/vendas.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Produtos - Sissi Semi Joias e Acessórios</title>
+  <link rel="stylesheet" href="styles/global.css">
+  <link rel="stylesheet" href="styles/produtos.css" />
+</head>
+<body>
+
+  <div class="container">
+    <div class="card app">
+
+      <aside class="sidebar">
+        <h2>Sissi Semi Joias e Acessórios</h2>
+
+        <nav>
+          <a href="/paineldecontrole">Painel de Controle</a>
+          <a href="produtos.php">Produtos</a>
+          <a href="vendas.php" class="active">Vendas</a>
+          <a href="relatorios.php">Relatórios</a>
+          <a href="estoque.php">Estoque</a>
+          <a href="usuarios.php">Controle de Usuários</a>
+          <a href="impressoras.php">Impressoras</a>
+          <a href="fornecedores.php">Fornecedores</a>
+        </nav>
+      </aside>
+
+      <main class="main">
+
+
+      </main>
+
+    </div>
+  </div>
+</body>
+</html>
